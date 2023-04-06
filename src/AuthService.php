664ee4a01f0852036f3a40777ba3ddb1c08e9aac
Mike Barlund <michaelbarlund@lifemd.com>
2023-04-06 15:04:20 -0700

    Expire the token in Redis 10 minutes before the token actually expires

--- src/AuthService.php
+++ src/AuthService.php
@@ -116,10 +116,12 @@ class AuthService
      * @param string $access_token
      * @param int $expires_in
      * @return void
+     * @throws \RedisException
      */
     private function storeAccessToken(string $access_token, int $expires_in): void
     {
-        $this->cache->set($this->access_token_key, $access_token, $expires_in);
+        // Expire the token in Redis 10 minutes before the token actually expires
+        $this->cache->set($this->access_token_key, $access_token, $expires_in-600);
     }
 
     /**
